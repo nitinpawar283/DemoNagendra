9f437ea31ad95e85f95f98244e1a6f75de47e49f
mishra2306 <n.mishra2306@gmail.com>
2017-04-30 19:41:43 +0530

    1st Time Add by TL

--- DemoForNagendra/src/com/nitin/Hello.java
+++ DemoForNagendra/src/com/nitin/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Nagendra should we close our Lapi Now");
+		System.out.println("Hello Nitin When will you go to cutting...");
 	}
 
 }
